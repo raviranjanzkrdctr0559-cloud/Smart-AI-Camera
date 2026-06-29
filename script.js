b02710ee300a7ba79eb0766c834a6d48daedc3af
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-29 23:10:00 +0530

    Ai camera

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ alert("Camera Access Denied");
 }
 
 startCamera();
+const canvas = document.getElementById("canvas");
+const captureBtn = document.getElementById("capture");
+
+captureBtn.addEventListener("click", () => {
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0);
+
+    const link = document.createElement("a");
+    link.download = "photo.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+
+});
