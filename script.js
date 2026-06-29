d444611b975c49f77a32e474c8aeae1e6301b017
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-29 23:17:48 +0530

    Ai camera

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+
+const captureBtn = document.getElementById("capture");
 const switchBtn = document.getElementById("switch");
 
 let currentCamera = "environment";
 let currentStream;
 
+// Camera Start
 async function startCamera() {
 
+    // Purana camera band karo
     if (currentStream) {
         currentStream.getTracks().forEach(track => track.stop());
     }
@@ -12,24 +18,20 @@ async function startCamera() {
     try {
 
         currentStream = await navigator.mediaDevices.getUserMedia({
-
             video: {
                 facingMode: currentCamera
             },
-            audio: true
-
+            audio: false
         });
 
         video.srcObject = currentStream;
 
     } catch (err) {
-
-        alert("Camera Error");
-
+        alert("Camera Error: " + err.message);
     }
-
 }
 
+// Camera Switch
 switchBtn.addEventListener("click", () => {
 
     currentCamera =
@@ -40,3 +42,22 @@ switchBtn.addEventListener("click", () => {
     startCamera();
 
 });
+
+// Photo Capture
+captureBtn.addEventListener("click", () => {
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0);
+
+    const link = document.createElement("a");
+    link.download = "photo.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+
+});
+
+// App Start
+startCamera();
