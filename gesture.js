942fee12f768a460d05dcfa7ff7b348e964d21c0
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-30 17:59:35 +0530

    Ai camera

--- gesture.js
+++ gesture.js
@@ -12,3 +12,41 @@ async function startAI() {
 
 startAI();
 console.log("Gesture.js Loaded");
+const hands = new Hands({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    }
+});
+
+hands.setOptions({
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.7,
+    minTrackingConfidence: 0.7
+});
+
+hands.onResults(onResults);
+
+function onResults(results){
+
+    if(results.multiHandLandmarks.length > 0){
+
+        statusBox.innerHTML = "✋ Hand Detected";
+
+    }else{
+
+        statusBox.innerHTML = "🤖 AI Ready";
+
+    }
+
+}
+
+const camera = new Camera(video,{
+    onFrame: async()=>{
+        await hands.send({image:video});
+    },
+    width:640,
+    height:480
+});
+
+camera.start();
