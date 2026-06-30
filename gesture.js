5e76b4f756c3531483d1206c3e346b42a19072ad
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-30 17:37:37 +0530

    Ai camera

---
+++ gesture.js
@@ -0,0 +1,13 @@
+// Smart AI Camera Gesture System
+
+const statusBox = document.getElementById("status");
+
+statusBox.innerHTML = "🤖 AI Loading...";
+
+async function startAI() {
+
+    statusBox.innerHTML = "✋ AI Ready";
+
+}
+
+startAI();
