8befd9cbd72c64fa8cd7c4fd1cc68bbab2e4834e
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-30 23:01:14 +0530

    Ai camera

--- gesture.js
+++ gesture.js
@@ -1,17 +1,10 @@
 // Smart AI Camera Gesture System
 
 const statusBox = document.getElementById("status");
+const videoElement = document.getElementById("video");
 
 statusBox.innerHTML = "🤖 AI Loading...";
 
-async function startAI() {
-
-    statusBox.innerHTML = "✋ AI Ready";
-
-}
-
-startAI();
-console.log("Gesture.js Loaded");
 const hands = new Hands({
     locateFile: (file) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
@@ -27,18 +20,15 @@ hands.setOptions({
 
 hands.onResults(onResults);
 
-function onResults(results){
+function onResults(results) {
 
- if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
-
-    statusBox.innerHTML = "✋ Hand Detected";
-
-} else {
-
-    statusBox.innerHTML = "🤖 AI Ready";
+    if (results.multiHandLandmarks && results.multiHandLandmarks.length > 0) {
+        statusBox.innerHTML = "✋ Hand Detected";
+    } else {
+        statusBox.innerHTML = "🤖 AI Ready";
+    }
 
 }
-const videoElement = document.getElementById("video");
 
 const camera = new Camera(videoElement, {
     onFrame: async () => {
@@ -47,11 +37,7 @@ const camera = new Camera(videoElement, {
     width: 640,
     height: 480
 });
-    onFrame: async()=>{
-        await hands.send({image:video});
-    },
-    width:640,
-    height:480
-});
 
 camera.start();
+
+console.log("Gesture.js Loaded");
